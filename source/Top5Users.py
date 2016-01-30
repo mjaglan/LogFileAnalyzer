ab8c3794fd3707b8789d4bf2cf97948c7c9134d6
mjaglan <mjaglan@indiana.edu>
2016-01-30 16:58:00 -0500

    initial approach

--- source/Top5Users.py
+++ source/Top5Users.py
@@ -1,2 +1,18 @@
 def getTopKUsers():
+    '''
+    APPROACH #1: Handle large file and do computation on single machine
+        T = ~ O(n)
+
+    - Pass 1: make dictionary of (K=IP, V=count)
+            Doable in O(n) time
+
+    - Pass 2: make dictionary of (K=count, V=list(IP))
+            Doable in O(n) time, O(k) space
+    OR
+    - Pass 2: Maintain a min-count heap (count,IP) of size k, replace min-count head-node with larger value node, do heapify
+            Doable in O(n*logk) but general case will be quiet below this, O(k) space
+
+
+    APPROACH #2: Break large file and do Map-Reduce jobs (try later)
+    '''
     pass
