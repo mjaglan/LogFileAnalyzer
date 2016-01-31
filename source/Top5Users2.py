6209ca35a0f7868827a8ba1ac3b3e82f8db3c5d4
mjaglan <mjaglan@indiana.edu>
2016-01-31 12:42:57 -0500

    approach #2: chunks, code in progress

--- source/Top5Users2.py
+++ source/Top5Users2.py
@@ -15,7 +15,6 @@
     APPROACH #2: Break large file and do Map-Reduce jobs (try later)
 '''
 
-import csv
 import os
 from os import listdir
 from os.path import isfile, join
@@ -24,7 +23,8 @@ import heapq
 # constant
 ONE_GIGABYTE = pow(2,30)
 
-
+# solution: http://stackoverflow.com/a/8009942
+# solution: http://stackoverflow.com/a/519653
 def readInChunks(fileObj, chunkSize=ONE_GIGABYTE):
     """
         Lazy function to read a file piece by piece.
@@ -38,18 +38,74 @@ def readInChunks(fileObj, chunkSize=ONE_GIGABYTE):
 
 
 def getTopKUsers(k=5):
-    IP_Count = dict()
-
     cwd = os.getcwd()
     dirPath = cwd+'/../logs'
+
+    dFullFilePath = join(dirPath, 'data_store1.txt') # 0
+    dictionaryFile1 = open(dFullFilePath,"a+")
+    dictionaryFile1.write("0.0.0.0" + ',' + "0" + '\n') # dummy write
+
+    dFullFilePath = join(dirPath, 'data_store2.txt') # 1
+    dictionaryFile2 = open(dFullFilePath,"a+")
+    dictionaryFile2.write("0.0.0.0" + ',' + "0" + '\n') # dummy write
+
+    dictObjList = [dictionaryFile1, dictionaryFile2]
+    rToggle = 0
+    wToggle = 1
+    IP_Count = dict() # local dictionary for current raw chunk
     onlyfiles = [f for f in listdir(dirPath) if (isfile(join(dirPath, f)) and f.endswith(".csv"))]
     for aFile in onlyfiles[1:]:
         fullFilePath = join(dirPath, aFile)
-        f = open(fullFilePath,"r")
-        for chunk in readInChunks(f):
-            print("\n\n")
-            print chunk[2]
-            # handle csv
+        fo = open(fullFilePath,"r")
+        for chunk in readInChunks(fo):
+            # read raw chunk
+            for line in chunk.splitlines():
+                ip = line.split(',')[1]
+                if (ip in IP_Count.keys()):
+                    IP_Count[ip] += 1
+                else:
+                    IP_Count[ip] = 1
+
+            # merge this chunk and write dictionary, TODO: how to put it first time?
+            for dChunk in readInChunks(dictObjList[rToggle]):
+                writeBuff = ''
+                for line in dChunk.splitlines():
+                    print(line)
+                    ip,cntStr = line.split(',')
+                    cnt = int(cntStr)
+                    if (ip in IP_Count.keys()):
+                        IP_Count[ip] += cnt
+                        writeBuff = writeBuff + ip + ',' + IP_Count[ip] + '\n'
+                    else:
+                        # WARNING: DO NOT add to IP_Count, otherwise memory overflow may happen!
+                        writeBuff = writeBuff + ip + ',' + cnt + '\n'
+                dictObjList[wToggle].write(writeBuff)
+
+            # update read-write turns swapped for next chunk
+            temp = rToggle
+            rToggle = wToggle
+            wToggle = temp
+
+            # clear local chunk dictionary
+            IP_Count.clear()
+
+    for dChunk in readInChunks(dictObjList[rToggle]):
+        for line in dChunk.splitlines():
+            ipn,cntStr = line.split(',')
+            cnt = int(cntStr)
+            print(ipn,cnt)
+
+    heap = []
+    for key in IP_Count.keys()[:k]:
+        heapq.heappush(heap, (IP_Count[key],key))
+
+    for key in IP_Count.keys()[k:]:
+        if (heap[0][0] < IP_Count[key]):
+            heapq.heappop(heap)
+            heapq.heappush(heap, (IP_Count[key], key))
+
+    for item in heap:
+        print(item)
 
 if __name__ == '__main__':
     getTopKUsers()
